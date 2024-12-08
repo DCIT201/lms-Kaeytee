f1a19dc6c8d9845907ceb2e271565821a7e7944d
kaeytee <austinbediako4@gmail.com>
2024-12-08 16:48:59 +0000

    update to final work

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,9 @@
-public class Book {
+package org.example;
 
-    // This is your working class.
-    
+/**
+ * @param yearPublished Add this field
+ */
+public record Book(String title, String author, int yearPublished) {
+    // Constructor
+    // Initialize the field
 }
